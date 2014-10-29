b8f8c9856bc0089b298051874339d9cde9f85213
iRuxu <17229398@qq.com>
2014-10-29 14:44:09 +0800

    css change

---
+++ js/index.js
@@ -0,0 +1,85 @@
+
+	$(function(){
+		//内容切换
+		$("h2").click(function(){
+			$(".content").css('display','none');
+			$("h2").removeClass('ctcat');
+			var i = $(this).index('h2');
+			var num = $(".content").eq(i);
+			$(num).css('display','block');
+			$(this).addClass('ctcat');
+		})
+		
+		//h3开关
+		$("#main").on('click','h3',function(){
+			$(this).next('div').slideToggle('slow');
+			var tg3 = $(this).children('span').html();
+			if(tg3=='-'){
+				$(this).children('span').html('+');}
+			else if(tg3=='+'){
+				$(this).children('span').html('-');;
+			};
+		});
+
+		//h4开关
+		$("#main").on('click','h4',function(){
+			$(this).next('ul').slideToggle('slow');
+			var tg4 = $(this).children('span').html();
+			if(tg4=='-'){
+				$(this).children('span').html('+');}
+			else if(tg4=='+'){
+				$(this).children('span').html('-');;
+			};
+		})
+
+		//tips显示
+		$("#main").on('mouseover','h4+ul>li',function(e){
+			$(this).children('span').css({
+					left:e.pageX+12+'px',
+					top:e.pageY+8+'px',
+					display:'block'
+				})
+			}).on('mouseout','h4+ul>li',function(){
+				$(this).children('span').css({
+					display:'none'
+				})
+			})
+
+		//关闭片段盒子
+		$("#closebox").click(function(){
+			$("#snippet").css('display','none');
+		})
+
+		//tips添加至snippet box
+		$("#main").on('click','h4+ul>li',function(){
+			var infos = $(this).children('span').html();
+			$("#snippet").css('display','block');
+			$("#box").html(infos);
+		})
+
+		//阻止默认行为
+		$("a:not([href^=http])").each(function(){
+			$(this).click(function(e){
+				e.preventDefault();
+			})
+		})
+
+		//直接link阻止冒泡
+		$("a[href^='http']").each(function(){
+			$(this).click(function(e){
+				e.stopPropagation();
+			})
+		})
+
+		//h6开关
+		$("footer").on('click','h6',function(){
+			$(this).next('ul').slideToggle('slow');
+			var tg6 = $(this).children('span').html();
+			if(tg6=='-'){
+				$(this).children('span').html('+');}
+			else if(tg6=='+'){
+				$(this).children('span').html('-');;
+			};
+		})
+
+	})
