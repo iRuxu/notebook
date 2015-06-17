3cbe23c15082030a1a212896a0a3fa0f80387858
iRuxu <17229398@qq.com>
2015-06-17 10:58:14 +0800

    修订css部分

--- origin/.silky/config.js
+++ origin/.silky/config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    //如果需要指定特殊的项目名称，请在此指定
+    //name: "your_project",
     //配置文件的版本，和silky的版本无关
     version: 0.2,
     //80端口在*nix下需要sudo
@@ -13,39 +15,27 @@ module.exports = {
         }
     },
     //路由
-    routers: [
-         {
-            //path: 原路径，to: 替换后的路径，next：是否继承执行下一个路由替换，static：是否为静态文件，静态文件直接返回
-            path: /^\/$/, to: 'index.html', next: true, static: true
-         }
-    ],
+    routers: [],
     //插件的配置
     plugins: {
-        /*
-        //为插件指定目录，可以指定特殊目录的插件
-        "specific_plugin": {
-            "source": "指定插件的源路径",
-            "priority": 1
-        }
-        */
+      /*"honey": {},
+      "honey-preview": {
+        "server": "108"
+      },*/
+      "autoprefix":{},
+      //用于上传示例图片的插件
+      "honey-image-demo-upload": {}
     },
     //build的配置
     build: {
         //构建的目标目录，命令行指定的优先
         output: "./build",
         //将要复制的文件目录，直接复制到目标
-        copy: [/^images(\-demo)?$/i],
+        copy: [],
         //完全忽略处理的文件
-        ignore: [/^template\/module$/i, /^css\/module$/i, /(^|\/)\.(.+)$/, /\.(log)$/i],
+        ignore: [],
         //重命名
-        rename: [
-            {
-                source: /source\.(js)$/i, target: '$1', next: false
-            },
-            {
-                source: /^template\/(.+)/i, target: '$1', next: false
-            }
-        ],
+        rename: [],
         //是否压缩
         compress: {
             //将要忽略压缩的文件
@@ -61,6 +51,3 @@ module.exports = {
         }
     }
 }
-/*
-    silky运行的配置文件
- */
--- origin/.silky/data/development/global.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    "title": "Silky",
-    "root": "",
-    "keyword": "deploy, frontend",
-    "description": "description"
-}
