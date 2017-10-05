9822a213960c9f6b9fdbae1bb42cf95e6132aa70
iRuxu <rx6@qq.com>
2017-10-06 02:11:31 +0800

    js_array

--- js/test.js
+++ js/test.js
@@ -1,139 +1,77 @@
-//var arr = [1,2,3,4,5,6,7,8];
+/*var arr = [2,3,4]
+var add = [5,6]
+var test = arr.concat(5,add)
+console.log(test)*/
 
-//map & forEach
-/*var add = function(num){
-	num += 1
-	//console.log(num)
-	return num
-}
-//var test = arr.forEach(add)
-var test = arr.map(add)
-console.log(test)
-*/
+/*var arr = ['a','b','c']*/
 
-//reduce
-/*var test = arr.reduceRight(function(total,val,index,arr){
-	console.log('' + total + '+' + val)
-	total += val
-	console.log('=' + total)
-	return total
+/*var test = arr.join('+')
+console.log(test)*/
+//var test = arr.unshift('d','x')
+//var test = arr.push('d','x')
+//var test = arr.pop('a')
+/*var test = arr.shift('b')*/
+
+/*var origin = ['a','b','c','d','e','f']
+console.log(origin)
+var arr = ['a','b','c','d','e','f']*/
+
+//var test = arr.slice(0,3)
+//var test = arr.slice(2)
+//var test = arr.slice(-2)
+/*var test = arr.slice(1)*/
+/*var o = ['z','y']*/
+
+//仅添加
+//var test = arr.splice(1,0,o)
+
+//仅删除
+/*console.log(arr[1])
+console.log(arr[2])
+var test = arr.splice(1,2)*/
+
+/*var test = arr.fill('test',1,2)*/
+
+//var test = arr.copyWithin(1,0,3)
+
+/*console.log(arr)
+console.log(test)*/
+
+
+/*var setDemo = new Set([1,2,3])
+console.log(setDemo[0])*/
+
+/*var arr = ['a','b','c','a']
+var test = arr.indexOf('d')
+console.log(test)*/
+
+/*var arr = [1,2,3,4,52532]
+var test = arr.findIndex(function(item){
+	return item < 0
 })
 console.log(test)*/
 
+/*function Student(name) {
+    this.name = name || '匿名'; // 默认值为'匿名'
+}
+var test = new Student('iRuxu')
+//console.log(typeof null)
+console.log(Student instanceof Function)*/
 
-//质数
-/*function isPrime(n){
-	if(n <= 3) return false
-	if(n % 2 == 0 || n % 3 == 0) return false
-	//console.log(Math.sqrt(n))
-	for(var i = 2 ; i<= Math.sqrt(n); i++){
-		if (n % i == 0){
-			return false
-			break
-		}else{
-			return true
-		}
-	}
-}*/
-/*var test = isPrime(11)
-console.log(test)*/
-/*var test = arr.filter(isPrime)
+/*var arr = [1,'a']*/
+/*var test = arr.toString(8)*/
+/*arr.push()
+var test = arr.valueOf()
 console.log(test)*/
 
-
-/*var arr = [2,124,12,23,21]
-var test = arr.reverse()
-console.log(arr)
+/*var str = 'foo'
+var test = Array.from(str)
 console.log(test)*/
 
-/*var arr = [2,78,11]
-var test = arr.find(function(item){
-	return item > 150
-})
+/*var arr = ['a', 'b', 'c'];
+
+var test = arr.includes('a', 1);   //false
 console.log(test)*/
 
-/*function count() {
-    var arr = [];
-    for (var i=1; i<=3; i++) {
-        arr.push(
-        	(function () {
-            	return i * i;
-        	})()
-        );
-    }
-    console.log(i)
-    console.log(arr)
-    return arr;
-}
-
-var results = count()*/
-/*var results = x => x*x
-console.log(results(2))*/
-
-/*window.test = 'ok'
-console.log(typeof window.aa === 'undefined')*/
-
-/*'use strict'
-function Person(name){
-	this.name = name;
-	this.hi = function (){console.log('hello, ' + name)}
-	console.log(this)
-}
-
-var test = Person()
-console.log(name)
-console.log(hi)*/
-/*var o = new Person('iRuxu')
-var test = o.constructor
-console.log(test)*/
-
-/*function Student(props) {
-    this.name = props.name || 'Unnamed';
-}
-
-Student.prototype.hello = function () {
-    alert('Hello, ' + this.name + '!');
-}
-
-function PrimaryStudent(props) {
-    // 调用Student构造函数，绑定this变量:
-    Student.call(this, props);
-    this.grade = props.grade || 1;
-}
-
-var test = PrimaryStudent({name:'ok'})
-console.log(test)
-
-function inherits(Child, Parent) {
-    var F = function () {};
-    F.prototype = Parent.prototype;
-    Child.prototype = new F();
-    Child.prototype.constructor = Child;
-}*/
-/*class Student {
-    constructor(name,number) {
-        this.name = name;
-        this.number = number;
-    }
-
-    hello() {
-        alert('Hello, ' + this.name + '!');
-    }
-}
-
-class PrimaryStudent extends Student{
-	constructor(name,grade){
-		super(name);
-		this.grade = grade;
-	}
-
-	go(){
-		console.log('ok~~')
-	}
-}
-
-var test = new PrimaryStudent('iRuxu',5)
-console.log(test)
-//console.log(test.hello())
-console.log(test.go())
-*/
+/*console.log(isFinite(1))*/
+console.log(Number.isInteger(1))
