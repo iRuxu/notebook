51b321f749350dc1cdd2616eb6f83e12e1a97319
iRuxu <rx6@qq.com>
2017-10-07 03:35:26 +0800

    js-标准对象，regexp

--- js/test.js
+++ js/test.js
@@ -1,51 +1,29 @@
-/*var str = 'abcdef'*/
-//var test = str.indexOf('b',3)
-/*var test = str.lastIndexOf('d',-2)*/
+/*var now = new Date()*/
+/*console.log(now) // Wed Jun 24 2015 19:49:22 GMT+0800 (CST)
+console.log(now.getFullYear()) // 2015, 年份
+console.log(now.getMonth()) // 5, 月份，注意月份范围是0~11，5表示六月
+console.log(now.getDate()) // 24, 表示24号
+console.log(now.getDay()) // 3, 表示星期三
+console.log(now.getHours()) // 19, 24小时制
+console.log(now.getMinutes()) // 49, 分钟
+console.log(now.getSeconds()) // 22, 秒
+console.log(now.getMilliseconds()) // 875, 毫秒数
+console.log(now.getTime()) // 1435146562875, 以number形式表示的时间戳
+*/
 
-/*var test = str.substring(1,-2)*/
+/*var d = new Date(2015, 5, 19, 20, 15, 30);
+var c = new Date(2015, 5, 19, 20, 15, 30);
+console.log(d.getTime())
+console.log(c.getTime())*/
 
+/*console.log(date.toLocaleString())
+console.log(date.toUTCString())*/
+/*console.log(date.getTime())
+console.log(Date.now())*/
 
-/*var str = 'apple++=hello,ok,thank'*/
-//var test = str.match('ao')
-//var test = str.search('a','o')
-/*var test = str.match(/a/)*/
-//var test = str.search(/a/)
-
-//var test = str.replace('a','x')
-//console.log(str.length)
-/*var test = str.replace(/a/g,function(arg1,arg2,arg3,arg4){
-	//console.log(arg1)
-	console.log(arg2)
-	//console.log(arg3)
-	//console.log(str.length)
-	if(str.length > 5){
-		return '_'
-	}else{
-		return 'aa'
-	}
-})*/
-
-/*var ok = '~~~'
-var test = str.concat(ok,'!!')*/
-/*var test = str.split(',',3)*/
-
-/*var ori = 88
-var test = String.fromCharCode(88)*/
-
-/*var str = 'apple++=hello,123q4?哈ok,thank'
-var test = str.normalize(NFKC)*/
-
-/*var str = 'a'
-var test = str.repeat(5)*/
-
-var str = 'my%20test.php?name=st%C3%A5le&car=saab^%^$#%^^啊啊啊 ^^';
-/*var test1 = 'decode:' + decodeURI(str);
-var test2 = 'decodeC:' + decodeURIComponent(str);*/
-var test3 = 'es:' + escape(str);
-var test4 = 'unes:' + unescape(str);
-
-console.log(str)
-/*console.log(test1)
-console.log(test2)*/
-console.log(test3)
-console.log(test4)
+var str = 'ello,elxx'
+var exp = /el/g
+console.log(exp.exec(str))
+console.log(exp.lastIndex)
+console.log(exp.exec(str))
+console.log(exp.lastIndex)
