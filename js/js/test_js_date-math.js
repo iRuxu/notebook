ef81e7b58431f0c6c966a7a8878099d4be7039ad
iRuxu <rx6@qq.com>
2017-10-08 22:29:30 +0800

    Signed-off-by: iRuxu <rx6@qq.com>

---
+++ js/js/test_js_date-math.js
@@ -0,0 +1,29 @@
+/*var now = new Date()*/
+/*console.log(now) // Wed Jun 24 2015 19:49:22 GMT+0800 (CST)
+console.log(now.getFullYear()) // 2015, 年份
+console.log(now.getMonth()) // 5, 月份，注意月份范围是0~11，5表示六月
+console.log(now.getDate()) // 24, 表示24号
+console.log(now.getDay()) // 3, 表示星期三
+console.log(now.getHours()) // 19, 24小时制
+console.log(now.getMinutes()) // 49, 分钟
+console.log(now.getSeconds()) // 22, 秒
+console.log(now.getMilliseconds()) // 875, 毫秒数
+console.log(now.getTime()) // 1435146562875, 以number形式表示的时间戳
+*/
+
+/*var d = new Date(2015, 5, 19, 20, 15, 30);
+var c = new Date(2015, 5, 19, 20, 15, 30);
+console.log(d.getTime())
+console.log(c.getTime())*/
+
+/*console.log(date.toLocaleString())
+console.log(date.toUTCString())*/
+/*console.log(date.getTime())
+console.log(Date.now())*/
+
+var str = 'ello,elxx'
+var exp = /el/g
+console.log(exp.exec(str))
+console.log(exp.lastIndex)
+console.log(exp.exec(str))
+console.log(exp.lastIndex)
