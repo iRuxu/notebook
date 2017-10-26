468e4fd32df56217346c6a2df58d72bc3509fdbc
iRuxu <rx6@qq.com>
2017-10-27 01:09:37 +0800

    js book - update function

--- js/test.js
+++ js/test.js
@@ -1,12 +1,14 @@
 'use strict'
 
+function foo(i){
+	console.log('函数:'+i)
+	return function (){
+		console.log('闭包:'+i)
+		return ++i
+	}
+}
 
-jQuery(function($){
-
-});
-
-var print = function x(){
-  console.log(typeof x);
-};
-
-print()
+var inc = foo(1)
+console.log(inc())
+console.log(inc())
+console.log(inc())
