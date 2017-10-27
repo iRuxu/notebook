a51b11ae916b173b002529554fb9b235f2ec460e
iRuxu <rx6@qq.com>
2017-10-27 19:01:25 +0800

    js book - update object

--- js/test.js
+++ js/test.js
@@ -1,14 +1,5 @@
 'use strict'
-
-function foo(i){
-	console.log('函数:'+i)
-	return function (){
-		console.log('闭包:'+i)
-		return ++i
-	}
+for(var i = 0; i < 5; i++){
+  console.log(i);
+  if (i === 2) debugger;
 }
-
-var inc = foo(1)
-console.log(inc())
-console.log(inc())
-console.log(inc())
