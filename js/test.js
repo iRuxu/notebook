aafecd11e00fdfc5b0bb85917e14214bfeaf1895
iRuxu <rx6@qq.com>
2017-11-30 19:47:32 +0800

    html book - event

--- js/test.js
+++ js/test.js
@@ -1,4 +1,5 @@
-var test = setTimeout(function (){
-	console.log('ok')
-},100)
-console.log(test)
+// html代码为 <p id="p">foobar</p>
+var p = document.getElementById('p');
+var test = p.firstChild;
+var newText = test.splitText(3);
+console.log(newText)
