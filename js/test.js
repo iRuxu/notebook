cc95d5e09bd4f1d0b09f57f657a7f8c88dd9a668
iRuxu <rx6@qq.com>
2017-12-05 18:36:42 +0800

    html5 book - H5 API

--- js/test.js
+++ js/test.js
@@ -1,6 +1,23 @@
-var body = document.body
-body.onfocus = function (e){
-	console.log('ok')
-	console.log(e.target)
-	console.log(e.relatedTarget)
+/*window.addEventListener('message', function(e) {
+  console.log(e.data);
+  console.log(e.source);
+  console.log(e.target);
+},false);
+
+var test2 = window.open('http://localhost:88/test2.html');
+test2.postMessage('Hello World!', 'http://localhost:88/test2.html');
+*/
+
+/*window.onload=function() {
+    window.frames[0].postMessage('收到没', '*');//在这里发送数据
+}*/
+
+/*var test2 = window.open('http://localhost:88/test2.html');
+test2.postMessage('Hello World!', '*');*/
+
+if(window.Notification && Notification.permission !== "denied") {
+	Notification.requestPermission(function(status) {
+		alert('请开启通知!')
+		var n = new Notification('通知标题', { body: '这里是通知内容！' }); 
+	});
 }
---
+++ js/test2.js
@@ -0,0 +1,9 @@
+/*window.addEventListener('message', function(e) {
+  alert(e.data);
+  alert(e.source);
+  alert(e.target);
+},false);*/
+
+ window.addEventListener("message",function(e){
+    alert(e.data);
+})
