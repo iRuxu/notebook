6f48c80efbf6699efc177966834f91ea7614f4ec
iRuxu <rx6@qq.com>
2017-12-20 18:57:21 +0800

    es6 book - class

--- js/test.js
+++ js/test.js
@@ -1,2 +1,10 @@
-var test = `\`Yo\` World!`;
+var someString = "hi";
+typeof someString[Symbol.iterator]
+// "function"
+
+var iterator = someString[Symbol.iterator]();
+
+var test = iterator.next()  // { value: "h", done: false }
 console.log(test)
+iterator.next()  // { value: "i", done: false }
+iterator.next()  // { value: undefined, done: true }
